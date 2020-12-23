1f4371f2f579e8f770c6d958ff4f0a82ba42c501
synrom <s6maleiw@uni-bonn.de>
2020-12-23 17:14:25 +0100

    hook_mem_read_invalid fuer Verstaendnis

--- fuzzer.py
+++ fuzzer.py
@@ -9,12 +9,21 @@ def hook_syscall(uc,user_data):
 	print("eax = "+hex(eax))
 	exit(0)
 
-uc = unicorn_loader.AflUnicornEngine("MemoryDump",debug_print=True)
+def hook_mem_read_invalid(uc,access,address,size,value,user_data):
+	eip = uc.reg_read(UC_X86_REG_EIP)
+	print("eip at invalid read = "+hex(eip))
+	print("size = "+hex(size))
+
+
+uc = unicorn_loader.AflUnicornEngine("MemoryDump",debug_print=False)
 
 uc.hook_add(UC_HOOK_INSN,hook_syscall,arg1=UC_X86_INS_SYSCALL)
+uc.hook_add(UC_HOOK_MEM_READ_INVALID,hook_mem_read_invalid)
 
 eip = uc.reg_read(UC_X86_REG_EIP)
+print("feip = "+hex(eip))
 uc.emu_start(eip,0,0,count=1)
 
 eip = uc.reg_read(UC_X86_REG_EIP)
+print("starteip = "+hex(eip))
 uc.emu_start(eip,0,timeout=0,count=0)
